dd583d69e9f71ba04428f6d9dba95e0a6f853e00
João Otávio Peccia <joaootaviopeccia0@@gmail.com>
2025-01-14 16:35:01 -0300

    first commit

---
+++ cmd/api/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "github.com/gin-gonic/gin"
+
+func main() {
+	r := gin.Default()
+
+	r.GET("/healthy", func (c *gin.Context)  {
+		c.JSON(200, gin.H{
+			"sucess": true,
+		})
+	})
+
+	r.Run(":8080")
+}
